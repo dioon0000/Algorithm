3938b9ac14faa6aac582c6e7b60f3d485ff229c6
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2022-07-03 21:04:34 +0900

    Basic Sort
    O(N^2)

---
+++ Algorithm/Select Sort.cpp
@@ -0,0 +1,32 @@
+//Select Sort O(N^2)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int arr[10] ={3, 2, 7, 116, 62, 235, 1, 23, 55, 77};
+	int n=10;
+
+	for(int i=0; i<10; ++i){
+		int min_idx = i;
+
+		for(int j=i+1; j<10; ++j){
+			if(arr[min_idx] > arr[j])
+				min_idx = j;
+		}
+		swap(arr[min_idx], arr[i]);
+	}
+
+	/*
+	11~19줄의 코드는 아래와 같이 변형 가능
+	for(int i=0; i<10; ++i)
+		swap(*min_element(arr+i, arr+10), arr[i]);
+	*/
+
+	for(int i=0; i<10; ++i)
+		cout << arr[i] << ' ';
+
+	return 0;
+}
