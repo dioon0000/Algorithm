7f8c751d7b2c8d7d5d54f8653874b6a14f955e73
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2022-07-03 14:41:55 +0900

    Basic Sort
    O(N^2)

--- Algorithm/BubbleSort.cpp
+++ Algorithm/BubbleSort.cpp
@@ -1,23 +1,26 @@
-//버블 정렬
-//시간복잡도 O(N^2)
-#include <stdio.h>
-#define N 100
+//Bubble Sort O(N^2)
+#include <bits/stdc++.h>
+#define N 10
+using namespace std;
 
 int main(){
-  int arr[N], temp;
+    int arr[N], temp;
 
-  for(int i=0; i<N; i++)
-    scanf("%d",&arr[i]);
+    for(int i=0; i<N; i++)
+        cin >> arr[i];
 
-  for(int i=0; i<N-1; i++){ 
-    for(int j=0; j<N-1-i; j++){
-      //arr[j]<arr[j+1]이면 내림차순 정렬
-      if(arr[j]>arr[j+1]){
-        temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
-      }
+    for(int i=0; i<N-1; i++){
+        for(int j=0; j<N-1-i; j++){
+            //arr[j]<arr[j+1]이면 내림차순 정렬
+            if(arr[j]>arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
     }
-  }
-  return 0;
+
+    for(int i=0; i<N; ++i)
+        cout << arr[i] << ' ';
+    return 0;
 }
