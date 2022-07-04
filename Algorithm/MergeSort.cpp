e6abb70f9bfdb709d0bafc10819adeabf6e7cb19
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2022-07-04 23:06:11 +0900

    MergeSort
    O(NlogN)

--- Algorithm/MergeSort.cpp
+++ Algorithm/MergeSort.cpp
@@ -1,45 +1,53 @@
-#include <stdio.h>
+//MergeSort O(NlogN)
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[10] ={10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+int n = 10;
 
 void Merge(int list[], int left, int mid, int right){
-	int i, j, k, l, temp[10];
+	int temp[10], i, j, k;
 	i = left;
 	j = mid+1;
-	k = left;
-	
-	while(i<=mid && j<=right){ //왼쪽 배열 값이 mid를 초과하거나 오른쪽 배열 값이 right를 초과하면 종료
+	k = left; //k는 temp 배열체크할때만 사용
+
+	while(i<=mid && j<=right){
 		if(list[i] <= list[j])
 			temp[k++] = list[i++];
 		else
 			temp[k++] = list[j++];
 	}
-  
-  while(i<=mid) 
-	  temp[k++] = list[i++];
-  while(j<=right) 
-	  temp[k++] = list[j++];
-  for(int x = left; x<=right; x++) 
-	  list[x] = temp[x];
-  
+
+	while(i<=mid) //13줄 while문에서 비교를 마치고 남아있는 왼쪽 배열의 가장 큰 값을 temp 배열에 삽입
+		temp[k++] = list[i++];
+
+	while(j<=right) //13줄 while문에서 비교를 마치고 남아있는 오른쪽 배열의 가장 큰 값을 temp 배열에 삽입
+		temp[k++] = list[j++];
+
+	for(int x = left; x<=right; ++x) //temp배열에 넣어두었던 값을 다시 list배열에 넣는 과정
+		list[x] = temp[x];
 }
 
 void MergeSort(int list[], int left, int right){
 	int mid;
-	
-	if(left<right){
+
+	//left값이 right값보다 작을때만 Divide 함수를 호출해줌
+	if(left < right){
 		mid = (left+right)/2;
+
 		MergeSort(list, left, mid);
 		MergeSort(list, mid+1, right);
 		Merge(list, left, mid, right);
 	}
+
+	return;
 }
 
-
 int main(){
-	int arr[10]={10,9,8,7,6,5,4,3,2,1};
-	
-	MergeSort(arr,0,9);
-	
-	for(int i=0; i<10; i++)
-		printf("%d ",arr[i]);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	Divide(arr, 0, n-1);
+	for(int i=0; i<n; ++i)	cout << arr[i] << ' ';
 	return 0;
 }
