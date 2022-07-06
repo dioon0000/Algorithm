1193be907a7dee6bc1e4528528d75e990c24a0fc
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2022-07-06 12:51:18 +0900

    QuickSort

--- Algorithm/QuickSort.cpp
+++ Algorithm/QuickSort.cpp
@@ -8,7 +8,7 @@ int n = 10;
 void quick_sort(int list[], int start, int end){
     // 원소가 1개인 경우 
     if(start>=end)    return;
-    int pivot = start;
+    int pivot = list[start];
     // 왼쪽 출발 지점
     int l = start+1;
     // 오른쪽 출발 지점 
@@ -17,15 +17,15 @@ void quick_sort(int list[], int start, int end){
     // 포인터가 엇갈릴때까지 반복
     while(1){
         //pivot값보다 list[l]값이 크지않다면 l값을 오른쪽으로 옮김
-        while(l<=end && list[pivot]>=list[l])
+        while(l<=end && pivot>=list[l])
             l++;
         //pivot값보다 list[r]값이 작지않다면 r값을 왼쪽으로 옮김
-        while(r>start && list[pivot]<=list[r])
+        while(r>start && pivot<=list[r])
             r--;
 
         //엇갈렸을때
         if(l>r){
-            swap(list[pivot], list[r]);
+            swap(list[start], list[r]);
             break;
         }
         //l과 r이 pivot기준 정상범위 내에서 멈췄을 때
