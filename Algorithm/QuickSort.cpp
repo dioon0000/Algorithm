373cdd54505b3d91c6acb9ef7aaf3429f04991e3
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2022-07-06 12:48:28 +0900

    QuickSort
    O(NlogN)

--- Algorithm/QuickSort.cpp
+++ Algorithm/QuickSort.cpp
@@ -1,46 +1,48 @@
-#include <stdio.h> 
-int data[10] = {4, 1, 2, 3, 9, 7, 8, 6, 10, 5}; 
-void quick_sort(int *data, int start, int end){ 
-  if(start >= end){ 
+//QuickSort 평균 : O(NlogN), 최악 : O(N^2)
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[10] = {4, 1, 2, 3, 9, 7, 8, 6, 10, 5};
+int n = 10;
+
+void quick_sort(int list[], int start, int end){
     // 원소가 1개인 경우 
-    return;
-  } 
-  int pivot = start; 
-  int i = pivot + 1; 
-  // 왼쪽 출발 지점 
-  int j = end; 
-  // 오른쪽 출발 지점 
-  int temp; 
-  while(i <= j){ 
-  // 포인터가 엇갈릴때까지 반복 
-    while(i <= end && data[i] <= data[pivot]){
-      i++; 
-    } 
-    while(j > start && data[j] >= data[pivot]){
-      j--; 
-    } 
-    if(i > j){ 
-      // 엇갈림 
-      temp = data[j]; 
-      data[j] = data[pivot]; 
-      data[pivot] = temp; 
+    if(start>=end)    return;
+    int pivot = start;
+    // 왼쪽 출발 지점
+    int l = start+1;
+    // 오른쪽 출발 지점 
+    int r = end;
+
+    // 포인터가 엇갈릴때까지 반복
+    while(1){
+        //pivot값보다 list[l]값이 크지않다면 l값을 오른쪽으로 옮김
+        while(l<=end && list[pivot]>=list[l])
+            l++;
+        //pivot값보다 list[r]값이 작지않다면 r값을 왼쪽으로 옮김
+        while(r>start && list[pivot]<=list[r])
+            r--;
+
+        //엇갈렸을때
+        if(l>r){
+            swap(list[pivot], list[r]);
+            break;
+        }
+        //l과 r이 pivot기준 정상범위 내에서 멈췄을 때
+        else{
+            swap(list[l], list[r]);
+            break;
+        }
     }
-    else{
-      // i번째와 j번째를 스왑 
-      temp = data[i]; 
-      data[i] = data[j]; 
-      data[j] = temp; 
-    } 
-  } 
-  // 분할 계산 
-  quick_sort(data, start, j - 1);
-  quick_sort(data, j + 1, end); 
+    // 분할 계산 
+    quick_sort(list, start, r-1);
+    quick_sort(list, r+1, end);
 } 
-  int main(void){
-    quick_sort(data, 0, 9); 
-    // 결과 확인 
-    for(int i=0; i<10; i++){
-      printf("%d ", data[i]); 
-    } 
+  int main(){
+    quick_sort(arr, 0, n-1); 
+
+    // Result Check
+    for(int i=0; i<10; i++) cout << arr[i] << ' ';
+
     return 0; 
   }
