bce138bbc5553727c74cd35f1a71e46b2bafdb2e
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2022-07-06 12:57:46 +0900

    Counting Sort

--- Algorithm/CountingSort.cpp
+++ Algorithm/CountingSort.cpp
@@ -1,22 +1,24 @@
-//Counting Sort
-#include <stdio.h>
-#define MAX 10000
+//Counting Sort O(N)
+#include <bits/stdc++.h>
+#define MAX 1000
+using namespace std;
 int main() {
-  int n, temp;
-  int arr[MAX]={0,};
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, temp;
+    int arr[MAX]={0, };
 
-  scanf("%d",&n);
-  for(int i=0; i<n; i++){
-    scanf("%d",&temp);
-    arr[temp-1]++;
-  }
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> temp;
+        arr[temp-1]++;
+    }
 
-  for(int i=0; i<MAX; i++){
-    //배열의 크기가 0일때, for문 생략을 위한 조건문
-    if(arr[i]!=0)
-      for(int j=0; j<arr[i]; j++){
-        printf("%d ",i+1);
-      }
-  }
-  return 0;
+    for(int i=0; i<MAX; i++)
+        //배열의 크기가 0일때, for문 생략을 위한 조건문
+        if(arr[i]!=0)
+            for(int j=0; j<arr[i]; j++)
+                cout << i+1 << ' ';
+
+    return 0;
 }
